ff79822ec4fc445acdcb00e96cc1b9238623a887
Thiago Vazquez Enninger <thiagoenninger@gmail.com>
2022-06-18 10:06:58 -0300

    criação das estilizações padrões do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header/>
       <Post author="Thiago Enninger" content="Aqui vai o conteúdo do autor" />
       <Post author="Stephanie Echevarria" content="Outro post muito positivo"/>
       <Post />
