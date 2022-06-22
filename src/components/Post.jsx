5c9f8911667364f205b919587ecc0274968da423
Thiago Vazquez Enninger <thiagoenninger@gmail.com>
2022-06-21 21:40:59 -0300

    atualização componente Post 21.06

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -23,6 +23,16 @@ export function Post(){
           <p><a href="#">jane.design/doctorcare</a></p>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea
+          placeholder="Deixe um comentário"/>
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
     </article>
   )
 }
